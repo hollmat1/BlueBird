da463345e435cafb12e3b63f61d9573e9b874045
Matthew Holland <matty.holland@gmail.com>
2023-04-06 21:52:03 +0200

    added subscription roles

---
+++ bbApi.Tests/AzureRMRBACTests.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using bbApi.App.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bbApi.Tests
+{
+    [TestClass]
+    public class AzureRMRBACTests
+    {
+        protected static IMapper _mapper;
+        protected AzureRMRBACService azureRMRBACService;
+
+   
+        [TestMethod]
+        public void Can_Get_Subscription()
+        {
+            var result = azureRMRBACService.GetSubscription("sdsd");
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Can_Get_Subscription_Role_Definitions()
+        {
+            var result = azureRMRBACService.GetSubscriptionRole("sdsd");
+            Assert.IsNotNull(result);
+        }
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var builder = new ConfigurationBuilder()
+            .AddJsonFile($"appsettings.json", true, true)
+            .AddJsonFile($"appsettings.development.json", true, true)
+            .AddUserSecrets<GroupAndRoleTests>()
+            .AddEnvironmentVariables();
+            var configuration = builder.Build();
+
+            if (_mapper == null)
+            {
+                var mappingConfig = new MapperConfiguration(mc =>
+                {
+                    mc.AddProfile(new App.Models.AutoMapperProfile());
+                });
+                IMapper mapper = mappingConfig.CreateMapper();
+                _mapper = mapper;
+            }
+
+            azureRMRBACService = new AzureRMRBACService(_mapper, configuration);
+
+        }
+
+    }
+}
---
+++ bbApi/App/Services/AzureRMRBACService.cs
@@ -0,0 +1,86 @@
+﻿using AutoMapper;
+using Azure;
+using Azure.Core;
+using Azure.Core.Pipeline;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Graph;
+using Microsoft.Graph.ExternalConnectors;
+using Microsoft.Identity.Web;
+using System.Diagnostics;
+using System.Net;
+
+namespace bbApi.App.Services
+{
+    public class AzureRMRBACService
+    {
+        protected ArmClient client;
+        protected readonly IMapper mapper;
+        protected readonly IConfiguration configuration;
+
+        public AzureRMRBACService(IMapper mapper, IConfiguration configuration)
+        {
+            this.mapper = mapper;
+            this.configuration = configuration;
+            client = PrepareAuthenticatedClient();
+        }
+
+        public SubscriptionResource GetSubscription(string subscriptionId) 
+        {
+            SubscriptionResource subscription = client.GetDefaultSubscription();
+            return subscription;
+        }
+
+        public List<AuthorizationRoleDefinitionResource> GetSubscriptionRole(string subscriptionId)
+        {
+            SubscriptionResource subscription = client.GetDefaultSubscription();
+            var roleDefitions = subscription.GetAuthorizationRoleDefinitions().ToList();
+            return roleDefitions;
+        }
+
+        public ResourceGroupCollection GetResourceGroupsAsync(string subscriptionId)
+        {
+            SubscriptionResource subscription = client.GetDefaultSubscription();
+            return subscription.GetResourceGroups();
+        }
+
+        private ArmClient PrepareAuthenticatedClient()
+        {
+            if (client == null)
+            {
+                // Create Microsoft Graph client.
+                // https://learn.microsoft.com/en-us/graph/sdks/choose-authentication-providers?tabs=CS#client-credentials-provider
+                try
+                {
+                    MicrosoftIdentityOptions aadoptions = new MicrosoftIdentityOptions();
+                    configuration.GetSection("AzureAd").Bind(aadoptions);
+
+                    var proxyAddress = "http://localhost:8888";
+
+                    // Create a new System.Net.Http.HttpClientHandler with the proxy
+                    var handler = new HttpClientHandler
+                    {
+                        // Create a new System.Net.WebProxy
+                        // See WebProxy documentation for scenarios requiring
+                        // authentication to the proxy
+                        Proxy = new WebProxy(new Uri(proxyAddress))
+                    };
+
+                    TokenCredential credForTenant01 = new ClientSecretCredential(aadoptions.TenantId, aadoptions.ClientId, aadoptions.ClientSecret);
+                    client = new ArmClient(credForTenant01);
+
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"Could not create a graph client {ex}");
+                }
+            }
+
+            return client;
+        }
+
+    }
+}
